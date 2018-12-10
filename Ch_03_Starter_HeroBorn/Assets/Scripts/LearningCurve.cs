06d7619f739cbf2bba6e43fcc66e53db3420ccba
Harrison Ferrone <hferrone@gmail.com>
2018-12-10 16:30:30 +0000

    Add chapter 3 starter project

---
+++ Ch_03_Starter_HeroBorn/Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public int carDoors = 4;
+
+    public int firstNumber = 2;
+    public int secondNumber = 3;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(2 + 4);
+        Debug.Log(carDoors - 2);
+
+        AddNumbers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Adds the numbers.
+    /// </summary>
+    void AddNumbers()
+    {
+        Debug.Log(firstNumber + secondNumber);
+    }
+}
