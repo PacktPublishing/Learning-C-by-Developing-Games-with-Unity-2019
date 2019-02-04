9cd48638589adffe1ae888f6793a4612b4633c71
Harrison Ferrone <hferrone@gmail.com>
2019-02-04 12:38:38 +0000

    Add chapter 9 starter project

---
+++ Ch_09_Starter_HeroBorn/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour 
+{
+	private void OnTriggerEnter(Collider other)
+	{
+        if(other.name == "Player")
+        {
+            Debug.Log("Enemy detected!");
+        }
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+        if(other.name == "Player")
+        {
+            Debug.Log("Enemy out of range.");
+        }
+	}
+
+}
---
+++ Ch_09_Starter_HeroBorn/Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameBehavior : MonoBehaviour 
+{
+    public string labelText = "Collect all 4 items and win your freedom!";
+    public bool showWinScreen = false;
+    public int maxItems = 4;
+
+    private int _itemsCollected = 0;
+    public int Items
+    {
+        get { return _itemsCollected; }
+        set { 
+            _itemsCollected = value;
+
+            if (_itemsCollected >= maxItems)
+            {
+                labelText = "You've found all the items!";
+                showWinScreen = true;
+            }
+            else
+            {
+                labelText = "Item found, only " + (maxItems - _itemsCollected) + " more to go!";
+            }
+        }
+    }
+
+    private int _playerLives = 3;
+    public int Lives 
+    {
+        get { return _playerLives; }
+        set { 
+            _playerLives = value; 
+            Debug.LogFormat("Lives: {0}", _playerLives);
+        }
+    }
+
+	void OnGUI()
+	{
+        GUI.Box(new Rect(20, 20, 150, 25), "Player Health: " + _playerLives);
+        GUI.Box(new Rect(20, 50, 150, 25), "Items Collected: " + _itemsCollected);
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 300, 50), labelText);
+
+        if (showWinScreen)
+        {
+            if (GUI.Button(new Rect(Screen.width/2 - 100, Screen.height/2 - 50, 200, 100), "YOU WON!"))
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+	}
+}
